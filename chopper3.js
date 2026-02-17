10231db49d66ed89071eda6be6841791a43e4418
Svet <70716489+ikyman@users.noreply.github.com>
2026-02-16 23:21:53 -0500

    Can now obtain choppers, removed Hot sauce resources

--- chopper3.js
+++ chopper3.js
@@ -15,19 +15,7 @@ func:function()
 		side:[],
 
 	}
-	new G.Res({
-		name:"tinned meal",
-		desc:"You, sitting on your computer might scoff at the idea of cold beans."+
-		"Your citizens, living a much less comfortable lives, are tickled pink by this acme of gastronomic experience."+
-		"Or, they would be, if they could actually open this oddly shiny cylinder.",
-		icon:[1,0,'spicySheet'],
-		turnToByContext:{'eat':{'health':0.01,'happiness':0.1},'decay':{'tinned meal':0.5}},
-		partOf:'food',
-		category:'food',
-	});
-
-	
-	//First we create a couple new resources :
+	// Vehicle Resources
 	new G.Res({
 		name:'unbladed chopper, no petrol',
 		desc:`A Motorcycle without any fuel on it. If you have a leather jacket, you could snap a pic for the 'gram.\
@@ -58,13 +46,23 @@ func:function()
 	});
 
 	new G.Res({
-		name:'hot sauce',
-		desc:'Made from [herb]s and the [hot pepper,Spiciest peppers], this [hot sauce] stays fresh for a while and will leave anyone panting and asking for more.',
+		name: "jerry-can",
+		desc: "Full of flammable fluid",
+		icon: [1,1,'spicySheet'],
+		partOf:'vehicles',
+	});
+
+	new G.Res({
+		name:"tinned meal",
+		desc:"You, sitting on your computer might scoff at the idea of cold beans."+
+		"Your citizens, living a much less comfortable lives, are tickled pink by this acme of gastronomic experience."+
+		"Or, they would be, if they could actually open this oddly shiny cylinder.",
 		icon:[1,0,'spicySheet'],
-		turnToByContext:{'eat':{'health':0.03,'happiness':0.1},'decay':{'hot sauce':0.95,'spoiled food':0.05}},//that last part makes hot sauce effectively have a 95% chance of simply not rotting (in effect, it decays into itself)
+		turnToByContext:{'eat':{'health':0.01,'happiness':0.1},'decay':{'tinned meal':0.5}},
 		partOf:'food',
 		category:'food',
 	});
+
 
 	new G.Res({
 		name:'FWOOOOSH',
@@ -81,12 +79,6 @@ func:function()
 	G.getDict('firekeeper').modes['fwooosh'] = {names:'wicked rad, yo!', desc: "Chuck petrol onto the fire. Frightens away choppers. (OSHA noncomplient)"}
 
 	G.getDict('firekeeper').effects.push({type:'convert',from:{'stick':40},into:{'fire pit':2, 'FWOOOOSH': 1},every:10,mode:'fwooosh'})
-
-	//Then we augment the base data to incorporate our new resources :
-		//adding a new mode to artisans so they can make hot sauce 
-	G.getDict('artisan').modes['hot sauce']={name:'Make hot sauce',desc:'Turn 3 and 3 [herb]s into 1 [hot sauce].',req:{'hot sauce preparing':true},use:{'knapped tools':1}};
-		//adding a new effect to artisans that handles the actual hot sauce preparing and is only active when the unit has the mode "hot sauce"
-	G.getDict('artisan').effects.push({type:'convert',from:{'herb':3},into:{'hot sauce':1},every:3,mode:'hot sauce'});
 	
 	G.getDict('wanderer').modes["unbladed chopper wanderer"] = {
 		name:'unbladed chopper wanderer',
@@ -119,18 +111,6 @@ func:function()
 		req:{'cooking':true},
 	});
 	
-	//Finally, we add a trait that amplifies the benefits of consuming hot sauce; it will take on average 20 years to appear once the conditions (knowing the "Hot sauce preparing" tech) is fulfilled.
-	new G.Trait({
-		name:'hot sauce madness',
-		desc:'@your people appreciate [hot sauce] twice as much and will be twice as happy from consuming it.',
-		icon:[1,1,'spicySheet'],
-		chance:20,
-		req:{'hot sauce preparing':true},
-		effects:[
-			{type:'function',func:function(){G.getDict('hot sauce').turnToByContext['eat']['happiness']=0.2;}},//this is a custom function executed when we gain the trait
-		],
-	});
-	
 	//There are many other ways of adding and changing content; refer to /data.js, the default dataset, if you want to see how everything is done in the base game. Good luck!
 
 	new G.Goods({
@@ -138,22 +118,32 @@ func:function()
 		desc: 'A [can stash]! Christmas came early!',
 		icon:[8,1],
 		res:{
-			'gather':{'tinned meal':1, 'gerry can': 0.2}
+			'gather':{'tinned meal':1, 'jerry-can': 0.2}
 		}
 	})
 	new G.Goods({
-		name:"abandoned Motorcycle",
+		name:"abandoned motorcycle",
 		desc: "The open road! Freedom! Before the end, it was a siren song.",
 		icon:[8,1],
 		res:{
 			'gather':{"unbladed chopper, no petrol": 2, "unbladed chopper":1}
 		}
 	})
+	new G.Goods({
+		name:"abandoned helicopter",
+		desc: "Finally, we figure out why all the children are spinning around while T-posing.",
+		icon:[8,1],
+		res:{
+			'gather':{"spinning chopper, no petrol": 2, "spinning chopper":0.5}
+		}
+	})
 	new G.Land({
 		name: "abandoned city",
-		names: ["abandoned city", "ruins from the time before", "ghost time"],
+		names: ["abandoned city", "ruins from the time before", "ghost town"],
 		goods:[
 			{type: 'can stash', amount:5},
+			{type: 'abandoned motorcycle', amount: 0.3},
+			{type: 'abandoned helicopter', amount: 0.1},
 			{type:['fir tree','oak','birch'],amount:0.2},
 			{type:'grass'},
 			{type:['wolves','bears'],chance:0.5,amount:1},
@@ -164,5 +154,8 @@ func:function()
 		image:9,
 		score:10
 	})
+
+	G.getDict["desert"].goods.push({type: 'abandoned motorcycle', amount: 0.1});
+	G.getDict["jungle"].goods.push({type: 'abandoned helicopter', amount: 0.1});
 }
 });
